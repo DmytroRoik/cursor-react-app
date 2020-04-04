d37f85937294c3803b366323c682d25f8a552277
Malfit <oleksandr.rybakov@nure.ua>
2020-04-04 13:44:48 +0300

    add-ui-for-categories-page

--- src/components/BtnAddMore/BtnAddMore.js
+++ src/components/BtnAddMore/BtnAddMore.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import Button from '../../../node_modules/@material-ui/core/Button';
-import './BtnAddMore.css';
+import Button from '@material-ui/core/Button';
+import './BtnAddMore.scss';
 
-const BtnAddMore = () => {
-
-    return (  
-        <div className = "btnAddMore">  
-            <Button variant="contained" color="primary">Add More</Button>
-        </div>  
-    )
-}
+const BtnAddMore = () => (
+  <div className="btnAddMore">
+    <Button variant="contained" color="primary">Add More</Button>
+  </div>
+);
 
 export default BtnAddMore;
--- src/components/TableCategories/TableCategories.js
+++ src/components/TableCategories/TableCategories.js
@@ -14,58 +14,58 @@ import Pets from '@material-ui/icons/Pets';
 import Restaurant from '@material-ui/icons/Restaurant';
 import Store from '@material-ui/icons/Store';
 import MoreVert from '@material-ui/icons/MoreVert';
+import './TableCategories.scss';
 
 const TableCategories = () => {
+  const useStyles = makeStyles({
+    table: {
+      minWidth: 600,
+    },
+  });
 
-    const useStyles = makeStyles({
-        table: {
-            minWidth: 600,
-        },
-    });
+  const createData = (category, description, date, action) => ({
+    category, description, date, action,
+  });
 
-    const createData = (category, description, date, action) => {
-        return { category, description, date, action };
-    }
+  const rows = [
+    createData(<span><RestaurantMenu />  Cook</span>, 'For all my food', '26/12/2019', <MoreVert />),
+    createData(<span><BeachAccess />  Clothes</span>, '', '23/12/2019', <MoreVert />),
+    createData(<span><Restaurant /> Restouraunts </span>, '', '22/12/2019', <MoreVert />),
+    createData(<span><Store /> Utility bills</span>, '', '21/12/2019', <MoreVert />),
+    createData(<span><Pets /> Pets</span>, '', '21/12/2019', <MoreVert />),
+  ];
 
-    const rows = [
-        createData(<span><RestaurantMenu/>  Cook</span> , "For all my food", "26/12/2019",<MoreVert/>),
-        createData(<span><BeachAccess/>  Clothes</span>, "" , "23/12/2019", <MoreVert/>),
-        createData(<span><Restaurant/> Restouraunts </span>, "", "22/12/2019",<MoreVert/>),
-        createData(<span><Store/> Utility bills</span>, "", "21/12/2019",<MoreVert/>),
-        createData(<span><Pets/> Pets</span>, "", "21/12/2019",<MoreVert/>),
-    ];
 
-    
-    const classes = useStyles();
+  const classes = useStyles();
 
-        return(
-            <TableContainer component={Paper}>
-            <Table className={classes.table} aria-label="simple table">
-                <TableHead>
-                <TableRow>
-                    <TableCell>Category</TableCell>
-                    <TableCell>Description</TableCell>
-                    <TableCell>Date</TableCell>
-                    <TableCell align="right">Action</TableCell>
-                   
-                </TableRow>
-                </TableHead>
-                <TableBody>
-                {rows.map((row) => (
-                    <TableRow key={row.category}>
-                    <TableCell component="th" scope="row">
-                        {row.category}
-                    </TableCell>
-                    <TableCell >{row.description}</TableCell>
-                    <TableCell >{row.date}</TableCell>
-                    <TableCell align="right">{row.action}</TableCell>
-                    
-                    </TableRow>
+  return (
+    <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Category</TableCell>
+            <TableCell>Description</TableCell>
+            <TableCell>Date</TableCell>
+            <TableCell align="right">Action</TableCell>
+
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map(row => (
+            <TableRow key={row.category}>
+              <TableCell component="th" scope="row">
+                {row.category}
+              </TableCell>
+              <TableCell >{row.description}</TableCell>
+              <TableCell >{row.date}</TableCell>
+              <TableCell align="right">{row.action}</TableCell>
+
+            </TableRow>
                 ))}
-                </TableBody>
-            </Table>
-            </TableContainer>
-        );
-    }
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
 
-    export default TableCategories;
+export default TableCategories;
