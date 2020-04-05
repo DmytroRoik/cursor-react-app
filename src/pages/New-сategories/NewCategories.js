1783bb8adc38ed9e5dea92d4205a407d101d815c
Nazar-Shtypka <nazar.schtypka@gmail.com>
2020-04-05 13:50:36 +0300

    corrected some problems with arrow func

--- src/pages/New-сategories/NewCategories.js
+++ src/pages/New-сategories/NewCategories.js
@@ -2,15 +2,14 @@ import React from 'react';
 import './NewCategories.scss';
 import FormsInput from './Forms';
 
-const NewCategories = () => {
-  return (
-    <div className="wrapper">
-      <div className="main__conrainer">
-        <div className="main__heading">New category</div>
-        <FormsInput />
-      </div>
+const NewCategories = () => (
+  <div className="wrapper">
+    <div className="main__conrainer">
+      <div className="main__heading">New category</div>
+      <FormsInput />
     </div>
-  );
-};
+  </div>
+);
 
 export default NewCategories;
+
