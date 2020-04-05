82893ffefef211a7fc561d780d63935a4883fe41
Nazar-Shtypka <nazar.schtypka@gmail.com>
2020-04-05 13:42:50 +0300

    created ui forms field

--- src/pages/New-сategories/Forms.js
+++ src/pages/New-сategories/Forms.js
@@ -3,24 +3,24 @@ import './NewCategories.scss';
 
 const FormsInput = () => {
   const [name, changeName] = useState();
+  const [description, changeDescription] = useState();
   function onChangeName(e) {
     changeName(e.target.value);
   }
-  const [description, changeDescription] = useState();
   function onChangeDescription(e) {
     changeDescription(e.target.value);
   }
   return (
     <div className="main-forms">
       <form className="main-forms__item" onSubmit={onChangeName}>
+        <p className="form__text">Name</p>
         <label>
-          <p className="form__text">Name</p>
           <input type="text" name="name" className="form__input" value={name} onChange={onChangeName} />
         </label>
       </form>
       <form className="main-forms__item" onSubmit={onChangeDescription}>
+        <p className="form__text">Description</p>
         <label>
-          <p className="form__text">Description</p>
           <input type="text" name="description" className="form__input" value={description} onChange={onChangeDescription} />
         </label>
       </form>
