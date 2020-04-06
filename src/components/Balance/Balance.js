24bcb0046adc7cc09886ce616de22622a2784ef9
Diana <diana_ivanivna@kr.net>
2020-04-06 21:24:59 +0300

    change name props for Balance and change style for class balance_box

--- src/components/Balance/Balance.js
+++ src/components/Balance/Balance.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 
-const Balance = ({ suma }) => (
+const Balance = ({ total }) => (
   <div className="balance">
     <div className="balance__box">
       <div className="balance__text">Balance</div>
-      <div className="balance__sum">{ suma }</div>
+      <div className="balance__sum">{ total }</div>
     </div>
   </div>
 );
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Balance from '../../components/Balance';
 
-const Home = () => <div><Balance suma={265279.07} /></div>;
+const Home = () => <div><Balance total={265279.07} /></div>;
 
 export default Home;
