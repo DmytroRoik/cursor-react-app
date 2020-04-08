1f9402158febb4273180492af18392410b38e1bc
Serhii <vasylyk29@gmail.com>
2020-04-08 17:11:41 +0300

    create tabs

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,46 @@
-import React from 'react';
-import Balance from '../../components/Balance';
+import React, { useState } from 'react';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 
-const Home = () => <div><Balance total={265279.07} /></div>;
+import Balance from '../../components/Balance';
+import './Home.scss';
+
+import '../../variables.scss';
+
+
+const Home = () => {
+  const [value, setValue] = useState('charges');
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className="home">
+      <Balance total={265279.07} />
+
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        aria-label="wrapped label tabs example"
+        TabIndicatorProps={{
+          style: {
+            backgroundColor: '#3b92f5',
+          },
+        }}
+      >
+        <Tab value="charges" label="Charges" className="home__tabs-title" />
+        <Tab value="income" label="Income" className="home__tabs-title" />
+      </Tabs>
+
+
+      <div value={value} hidden={value !== 'charges'}>
+        Item One
+      </div>
+      <div value={value} hidden={value !== 'income'}>
+        Item Two
+      </div>
+    </div>
+  );
+};
 
 export default Home;
