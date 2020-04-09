0374b134afec25884d9ecb3edd0871938117c6a2
Serhii <vasylyk29@gmail.com>
2020-04-09 14:05:45 +0300

    create select and add-btn

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,17 +1,23 @@
 import React, { useState } from 'react';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
+import Select from '@material-ui/core/Select';
 
+import BtnAddMore from '../../components/BtnAddMore';
 import Balance from '../../components/Balance';
 import './Home.scss';
-
 import '../../variables.scss';
-import SelectTimeOptions from '../../components/SelectorTimeOptions/SelectorTimeOptions'
 
 const Home = () => {
   const [value, setValue] = useState('charges');
   const handleChange = (event, newValue) => {
     setValue(newValue);
+  };
+
+  const [week, setWeek] = React.useState('week');
+
+  const handleWeek = (event) => {
+    setWeek(event.target.value);
   };
 
   return (
@@ -28,23 +34,33 @@ const Home = () => {
           },
         }}
       >
-        <Tab value="charges" label="Charges" className="home__tabs-title" /> 
+        <Tab value="charges" label="Charges" className="home__tabs-title" />
         <Tab value="income" label="Income" className="home__tabs-title" />
       </Tabs>
 
+      <div
+        className="home__select-row"
+        value={value}
+        hidden={value !== 'income'}
+      >
+        <div className="home__select">
+          <h3>My Charges</h3>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={week}
+            onChange={handleWeek}
+          >
+            <div value="week">this week</div>
+            <div value="month">this month</div>
+          </Select>
+        </div>
 
-      <div value={value} hidden={value !== 'charges'}> 
-        Item One  
-          <div> My {value} < SelectTimeOptions/></div>
+        <BtnAddMore />
       </div>
-      
-      <div value={value} hidden={value !== 'income'}> 
-        Item Two
-        <div> My {value} <SelectTimeOptions/> </div>
-      </div>
-    
     </div>
   );
 };
+
 
 export default Home;
