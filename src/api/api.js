bad173953fa420cf24ea451879aa0df2e19513ac
arbuzaicer <maxpachinov@gmail.com>
2020-04-13 16:20:44 +0300

    Add base API URL.

--- src/api/api.js
+++ src/api/api.js
@@ -1,10 +1,10 @@
 import * as axios from 'axios';
 
-const BASE_URL = '';
+const BASE_URL =
+  'http://ec2-35-181-53-65.eu-west-3.compute.amazonaws.com:3000/';
 const BASE_CONNECTION = axios.create({
   baseURL: BASE_URL,
 });
-
 
 const api = {};
 
