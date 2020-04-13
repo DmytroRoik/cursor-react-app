6cc143bdd2b0f9cbaac13c1e649276af3f407ed3
karinayurchenko <karina.yurchenko28@gmail.com>
2020-04-13 17:57:31 +0300

    updated for delete button when press cancel

--- src/components/BtnDeleteModal/BtnDeleteModal.js
+++ src/components/BtnDeleteModal/BtnDeleteModal.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
@@ -7,16 +7,20 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import './BtnDeleteModal.scss';
+import { actionSwitchDelete } from '../../redux/reducers/categories.reducer';
 
 export default function AlertDialog() {
-  
+  const key = useSelector(state => state.categoriesReducer.isDeleteEnable);
+  const deleteDispatch = useDispatch();
+
   return (
     <div>
       <Dialog
-       /* open={open}
-        
+        open={key}
+        onClose={() => deleteDispatch(actionSwitchDelete())}
+
         aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"*/
+        aria-describedby="alert-dialog-description"
       >
         <DialogTitle id="alert-dialog-title"><span className="dialog_title">Delete category?</span></DialogTitle>
         <DialogContent>
@@ -29,7 +33,7 @@ export default function AlertDialog() {
           <Button color="primary">
             OK
           </Button>
-          <Button onClick={handleClose} color="primary" autoFocus>
+          <Button onClick={() => deleteDispatch(actionSwitchDelete())} color="primary" autoFocus>
             Cancel
           </Button>
         </DialogActions>
