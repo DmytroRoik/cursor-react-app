0ebc343854ac91f33d49a7efc469c598da53594a
Dmytro Roik <dmytroroik@gmail.com>
2020-04-19 01:51:45 +0300

    [ADD] different environments

--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,7 @@
 import * as axios from 'axios';
 
-const BASE_URL =
-  'http://ec2-35-181-53-65.eu-west-3.compute.amazonaws.com:3000/';
 const BASE_CONNECTION = axios.create({
-  baseURL: BASE_URL,
+  baseURL: process.env.REACT_APP_BASE_URL,
 });
 
 const api = {
