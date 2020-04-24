2de5c85588fbbc673a21e8fdb7252d157ef2e936
Nazar-Shtypka <nazar.schtypka@gmail.com>
2020-04-24 20:15:41 +0300

    Corrected profile.selectors

--- src/redux/selectors/profile.selectors.js
+++ src/redux/selectors/profile.selectors.js
@@ -1,4 +1,3 @@
 const selectUserData = state => state.profileReducer.data;
-const selectBalance = state => state.profileReducer.totalBalance;
 
-export { selectUserData, selectBalance };
+export default selectUserData;
